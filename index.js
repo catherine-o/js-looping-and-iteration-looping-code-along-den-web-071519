bc775d6c8a9a3e5928ca3bbbc25c53b5c6004331
catherine-o <catherine.ohara@outlook.com>
2019-08-26 14:13:17 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let thanks = []
+    for (let i = 0; i < array.length; i++) {
+        thanks.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    };
+    return thanks;
+}
+
+
+function countdown(i) {
+    while (i > 0) {
+        console.log(i);
+        i -= 1;
+    }
+    console.log(i);
+}
